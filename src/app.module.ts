79747ddc0559db362310e18519b8fe12553454bf
Carlos-Francisco-Garcia-Flores <20221021@uthh.edu.mx>
2024-06-26 10:21:02 -0600

    Modificacion de los archivos referentes a la conexion con vercel, sin variable de entorno

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule } from '@nestjs/config';
+// import { ConfigModule } from '@nestjs/config';
 import { RegistrosModule } from './registros/registros.module';
 import { DocentesModule } from './docentes/docentes.module';
 import { AlumnosModule } from './alumnos/alumnos.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true, 
-    }),    MongooseModule.forRoot(process.env.MONGODB_URI),
+    MongooseModule.forRoot('mongodb+srv://Carlos:20221021@estadia.qahyork.mongodb.net/RegistroAsistencias?retryWrites=true&w=majority&appName=Estadia'),
     RegistrosModule,
     DocentesModule,
     AlumnosModule,
