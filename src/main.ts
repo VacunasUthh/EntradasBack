c8e350d37ab955f6f486fb23e1ae0104ede29637
Carlos-Francisco-Garcia-Flores <20221021@uthh.edu.mx>
2024-07-03 11:24:07 -0600

    habilitando cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cors from 'cors';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cors({
+    origin: '*', // Permitir todos los orígenes
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', // Permitir todos los métodos
+    allowedHeaders: 'Content-Type, Accept, Authorization', // Permitir todos los headers necesarios
+    credentials: true,
+  }));
   await app.listen(3000);
 }
 bootstrap();
